1773f894cd0a3825437ab8066dabf390d35f9aea
ivisheshrajput <ivisheshrajput@gmail.com>
2025-03-10 03:10:51 +0530

    Second Commit

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
@@ -11,12 +12,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className}`}>
-        <Header />
-        <main className="min-h-screen ">{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <ClerkProvider>
+      {" "}
+      <html lang="en">
+        <body className={`${inter.className}`}>
+          <Header />
+          <main className="min-h-screen ">{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
